0cf035faa821db901c7894f98c1829c1eeee0cc4
Andrew Yang <andy.tubeee@gmail.com>
2021-04-14 17:27:13 -0400

    Increased speed

--- assets/js/TypeItInit.js
+++ assets/js/TypeItInit.js
@@ -1,22 +1,24 @@
-setTimeout(() => {
-    new TypeIt("#hero-title-h1", {
-        strings: ["Better Learning", "Experience With", "Peak Education"],
-        speed: 120,
-        waitUntilVisible: true
-    })
-        .exec(async () => {
-            //-- Return a promise that resolves after something happens.
-            await new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    new TypeIt("#hero-subtitle-h2", {
-                        // strings: "Better Learning\nExperience With\nPeak Education",
-                        strings: "Founded in 2021",
-                        speed: 120,
-                        waitUntilVisible: true
-                    }).go();
-                    return resolve();
-                }, 1000);
-            });
+$(function(){
+    setTimeout(() => {
+        new TypeIt("#hero-title-h1", {
+            strings: ["Better Learning", "Experience With", "Peak Education"],
+            speed: 60,
+            waitUntilVisible: true
         })
-        .go();
-}, 900)
+            .exec(async () => {
+                //-- Return a promise that resolves after something happens.
+                await new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        new TypeIt("#hero-subtitle-h2", {
+                            // strings: "Better Learning\nExperience With\nPeak Education",
+                            strings: "Founded in 2021",
+                            speed: 80,
+                            waitUntilVisible: true
+                        }).go();
+                        return resolve();
+                    }, 1000);
+                });
+            })
+            .go();
+    }, 1100)
+})
