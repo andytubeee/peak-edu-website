e692453baa5b7c3f45d3b147a99c2723c68e3530
Andrew Yang <andy.tubeee@gmail.com>
2021-07-19 18:42:50 -0400

    Hour update + Carousel

---
+++ assets/js/Splide.js
@@ -0,0 +1,12 @@
+new Splide( '.splide', {
+    type   : 'loop',
+    perPage : 2,
+    autoplay: true,
+    interval: 3000,
+    padding: {
+        right: '5rem',
+        left : '5rem',
+    },
+    height: 1000,
+    fixedWidth: 500,
+} ).mount();
